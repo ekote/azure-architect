c57791646acb6c4e2ceb8ae070760e5bbde0d7b2
EK <esterakot@gmail.com>
2020-11-09 23:16:03 +0100

    imports

--- java-tensorflow/src/main/java/HelloTensorFlow.java
+++ java-tensorflow/src/main/java/HelloTensorFlow.java
@@ -1,11 +1,7 @@
-import org.tensorflow.ConcreteFunction;
-import org.tensorflow.Signature;
-import org.tensorflow.Tensor;
-import org.tensorflow.TensorFlow;
-import org.tensorflow.op.Ops;
-import org.tensorflow.op.core.Placeholder;
-import org.tensorflow.op.math.Add;
-import org.tensorflow.types.TInt32;
+import org.tensorflow.*;
+
+import java.util.UUID;
+
 
 public class HelloTensorFlow {
 
